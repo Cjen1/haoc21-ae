f14007ac6dd5e25ef4f0fc3619d544dab34fdbf3
Chris Jensen <cjjensen01@aol.com>
2021-04-20 18:50:03 +0100

    Lightweight tests

--- reckon_script.py
+++ reckon_script.py
@@ -63,15 +63,15 @@ for repeat in range(1):
     tag = 'repeat-{0}-bandwidth'.format(repeat)
     run_test(failure='leader', tag=tag)
     run_test(failure='partial-partition', tag=tag)
-    run_test(failure='intermittent-partial', duration='600', mtbf='10', tag=tag)
+#     run_test(failure='intermittent-partial', duration='600', mtbf='10', tag=tag)
     run_test(failure='intermittent-full', duration='600', mtbf='10', tag=tag)
     run_test(system='etcd-pre-vote', failure='partial-partition', tag=tag)
-    run_test(topo='wan', nn='7', tag=tag, tcp_dump = False)
+#     run_test(topo='wan', nn='7', tag=tag, tcp_dump = False)
 
-    tag = 'repeat-{0}'.format(repeat)
-    rates = [1,2000,4000,6000,8000,10000,12000,14000,16000,18000,20000,22000,24000,26000,28000,30000]
-    n_servers = [3,5,7,9]
-    params = [v for v in it.product(rates, n_servers)]
-    np.random.shuffle(params)
-    for rate, n_server in params:
-        run_test(nn=str(n_server), rate=str(rate), tag=(tag), tcp_dump=False)
+#     tag = 'repeat-{0}'.format(repeat)
+#     rates = [1,2000,4000,6000,8000,10000,12000,14000,16000,18000,20000,22000,24000,26000,28000,30000]
+#     n_servers = [3,5,7,9]
+#     params = [v for v in it.product(rates, n_servers)]
+#     np.random.shuffle(params)
+#     for rate, n_server in params:
+#         run_test(nn=str(n_server), rate=str(rate), tag=(tag), tcp_dump=False)
